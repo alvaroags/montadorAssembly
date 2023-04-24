872fd9bbb2aa69f1084f9a56baad990e6561eb1b
Esdraas <114784438+Esdraas@users.noreply.github.com>
2023-04-24 17:32:24 -0300

    Update entrada.asm

--- entrada.asm
+++ entrada.asm
@@ -1,2 +1,15 @@
-beqz x9, 100
-bnez x9, 100
+add x1, x2, x3
+sub x1, x2, x3
+and x1, x2, x3
+or x1, x2, x3
+xor x1, x2, x3
+sll x1, x2, x3
+srl x1, x2, x3
+bne x1, x2, 0x10
+beq x1, x2, 0x10
+sb x1, 0x10(x2)
+lb x1, 0x10(x2)
+sw x1, 0x10(x2)
+lw x1, 0x10(x2)
+sh x1, 0x10(x2)
+lh x1, 0x10(x2)
